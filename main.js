f9d0be552498ca905d99d094d223a07794beeedb
Chris <117781333+chrism-la@users.noreply.github.com>
2023-10-20 11:42:42 -0700

    JS: created square('div') with class name for styling purposes and ID for location purposes

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 const gameBoard = document.getElementById('gameBoard');
 const squares = [];
 
-for (let row = 0; row < 8; row++) {
-    for (let col = 0; col < 8; col++) {}
+createBoard();
+function createBoard() {
+    for (let row = 0; row < 8; row++) {
+        for (let col = 0; col < 8; col++) {
+            const square = document.createElement('div');
+            square.className = 'square';
+            square.id = `${row}-${col}`;
+            gameBoard.appendChild(square);
+        }
+    }
 }
