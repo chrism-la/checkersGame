aeed202c3e9f65be7a4b88a6a3e9ccf59308335c
Chris <117781333+chrism-la@users.noreply.github.com>
2023-11-02 12:57:16 -0700

    reverted

--- main.js
+++ main.js
@@ -94,9 +94,10 @@ function validMove(square, selectedPiece) {
         const jumpedRow = parseInt((selPieceRow + squareRow) / 2);
         const jumpedCol = parseInt((selPieceCol + squareCol) / 2);
         const jumpedSquare = document.getElementById(`${jumpedRow}-${jumpedCol}`);
-        if (currentPlayer === Player1 && selPieceRow === squareRow + 1 && (selPieceCol === squareCol + 1 || selPieceCol === squareCol - 1)) {
-            return true;
-        } else if (currentPlayer === Player2 && selPieceRow === squareRow - 1 && (selPieceCol === squareCol + 1 || selPieceCol === squareCol - 1)) {
+        if (
+            (selPieceRow === squareRow + 1 && (selPieceCol === squareCol + 1 || selPieceCol === squareCol - 1)) ||
+            (selPieceRow === squareRow - 1 && (selPieceCol === squareCol + 1 || selPieceCol === squareCol - 1))
+        ) {
             return true;
         } else if (jumpedSquare.querySelector('p')) {
             const capturedPiece = jumpedSquare.querySelector('p');
